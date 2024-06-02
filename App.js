1878a0b90bdbfc3b6886a17dcc19f0dc24c870f9
jarrenoh <jarrenoh@gmail.com>
2024-06-03 02:05:14 +0800

    removed page headers

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options= {{headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Pet" component={PetScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Home" component={HomeScreen} />
+        <Stack.Screen options= {{headerShown: false}} name="Pet" component={PetScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
