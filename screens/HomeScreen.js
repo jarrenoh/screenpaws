c5381659b19619af3185b07b4d9629b52c36fe95
niclow236 <e1155625@u.nus.edu>
2024-06-14 15:00:32 +0800

    hh:mm:ss instead of hh:mm

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -126,8 +126,9 @@ const HomeScreen = () => {
   const formatTime = (timeInSeconds) => {
     const hours = Math.floor(timeInSeconds / 3600);
     const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
     
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
   };
 
   return (
