8da9088d9ae21d9d84dffb7212ce8f82aeb3cab6
niclow236 <e1155625@u.nus.edu>
2024-07-10 18:57:01 +0800

    fixed bug in xp

--- screens/ShopScreen.js
+++ screens/ShopScreen.js
@@ -1,159 +1,189 @@
 import React, { useState, useEffect } from 'react';
-import { Image, StyleSheet, View, Text, TouchableOpacity, FlatList, SafeAreaView, Dimensions } from 'react-native';
+import { StyleSheet, View, Image, Text, Dimensions, TouchableOpacity } from 'react-native';
 import { auth, firestore } from '../firebase';
 import CustomNavbar from '../components/CustomNavbar';
+import { useNavigation } from '@react-navigation/native';
 import images from '../components/images';
 
-const ShopScreen = () => {
-  const [coins, setCoins] = useState(0);
-  const [items, setItems] = useState([]);
-  const [userItems, setUserItems] = useState([]);
+const PetScreen = () => {
+  const [xp, setXp] = useState(0);
+  const [level, setLevel] = useState(1);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [equippedItem, setEquippedItem] = useState(null);
+
+  const navigation = useNavigation();
 
   useEffect(() => {
     const fetchUserData = async () => {
       const user = auth.currentUser;
       if (user) {
-        const userDoc = await firestore.collection('users').doc(user.uid).get();
+        const userDocRef = firestore.collection('users').doc(user.uid);
+        const userDoc = await userDocRef.get();
         if (userDoc.exists) {
           const userData = userDoc.data();
-          setCoins(userData.coins || 0);
-          setUserItems(userData.items || []);
+          const userElapsedTime = userData.elapsedTime || 0;
+          setElapsedTime(userElapsedTime);
+          const userXp = userData.xp || Math.floor(userElapsedTime / 60); // 1 XP per minute
+          setXp(userXp);
+          const userLevel = userData.level || Math.floor(userXp / 10) + 1; // 10 XP per level
+          setLevel(userLevel);
+          const userEquippedItem = userData.equippedItem || null;
+          setEquippedItem(userEquippedItem);
         }
       }
     };
 
-    const fetchShopItems = async () => {
-      const itemsSnapshot = await firestore.collection('shopItems').get();
-      const shopItems = itemsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      setItems(shopItems);
-    };
-
     fetchUserData();
-    fetchShopItems();
   }, []);
 
-  const handleBuyItem = async (item) => {
-    // Check if the item is already purchased
-    if (userItems.some(userItem => userItem.id === item.id)) {
-      alert('You have already purchased this item.');
-      return;
-    }
-
-    if (coins >= item.price) {
-      const newCoins = coins - item.price;
-      const updatedUserItems = [...userItems, item];
-
-      setCoins(newCoins);
-      setUserItems(updatedUserItems);
-
+  useEffect(() => {
+    const updateUserData = async () => {
       const user = auth.currentUser;
       if (user) {
-        await firestore.collection('users').doc(user.uid).update({
-          coins: newCoins,
-          items: updatedUserItems
+        const userDocRef = firestore.collection('users').doc(user.uid);
+        const newUserXp = Math.floor(elapsedTime / 60);
+        const newUserLevel = Math.floor(newUserXp / 10) + 1;
+        await userDocRef.update({
+          xp: newUserXp,
+          level: newUserLevel,
+          elapsedTime,
         });
+        setXp(newUserXp);
+        setLevel(newUserLevel);
       }
-    } else {
-      alert('Not enough coins');
-    }
+    };
+
+    updateUserData();
+  }, [elapsedTime]);
+
+  const calculateXpPercentage = () => {
+    const currentLevelXp = (level - 1) * 10;
+    const nextLevelXp = level * 10;
+    const levelXpRange = nextLevelXp - currentLevelXp;
+    const xpIntoLevel = xp - currentLevelXp;
+    const xpPercentage = (xpIntoLevel / levelXpRange) * 100;
+    return isNaN(xpPercentage) ? 0 : xpPercentage;
   };
 
+  const xpBarContainerWidth = Dimensions.get('window').width * 0.8; // 80% of window width
+  const xpBarWidth = xpBarContainerWidth * (calculateXpPercentage() / 100);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.coinsContainer}>
-        <Image source={images.coin} style={styles.coinImage} />
-        <Text style={styles.coinsText}>Coins: {coins}</Text>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {equippedItem && <Image source={images[equippedItem.imageName]} style={styles.backgroundImage} />}
+        <Image source={level === 1 ? images.dog : level >= 5 ? images.swoledoge : images.placeholderImage} style={styles.image} />
+        <Text style={styles.text}>
+          {level === 1 ? 'Hungry Dog' : level >= 2 && level <= 5 ? 'Weak Doge' : 'Swole Doge'}
+        </Text>
+        <Text style={styles.text}>XP: {xp}</Text>
+        <Text style={styles.text}>Level: {level}</Text>
+        <View style={styles.xpBarContainer}>
+          <View style={[styles.xpBar, { flex: calculateXpPercentage() / 100 }]} />
+          <View style={{ flex: 1 - calculateXpPercentage() / 100 }} />
+          <Text style={styles.xpText}>Progress: {calculateXpPercentage().toFixed(0)}%</Text>
+        </View>
       </View>
-      <FlatList
-        data={items}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={styles.itemContainer}>
-            <Image 
-              source={images[item.imageName]} 
-              style={styles.itemImage} 
-              onError={(error) => console.log('Error loading image', error)}
-            />
-            <Text style={styles.itemText}>{item.name}</Text>
-            <Text style={styles.itemText}>{item.price}</Text>
-            <Image source={images.coin} style={styles.coinImage} />
-            <TouchableOpacity
-              style={[
-                styles.buyButton,
-                userItems.some(userItem => userItem.id === item.id) && styles.disabledButton
-              ]}
-              onPress={() => handleBuyItem(item)}
-              disabled={userItems.some(userItem => userItem.id === item.id)}
-            >
-              <Text style={styles.buyButtonText}>
-                {userItems.some(userItem => userItem.id === item.id) ? 'Bought' : 'Buy'}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
       <View style={styles.navbarContainer}>
         <CustomNavbar />
       </View>
-    </SafeAreaView>
+      <View style={styles.headerButtons}>
+        <TouchableOpacity onPress={() => navigation.navigate('Achievements')} style={styles.achievementButton}>
+          <Image source={images.achievement} style={styles.achievementIcon} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Inventory')} style={styles.inventoryButton}>
+          <Image source={images.inventory} style={styles.inventoryIcon} />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
-export default ShopScreen;
+export default PetScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#fff',
-  },
-  coinsContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 16,
+    justifyContent: 'center'
   },
-  coinImage: {
-    width: 24,
-    height: 24,
-    marginRight: 8,
+  headerButtons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: '100%',
+    padding: 16,
+    position: 'absolute',
+    top: 60,
+    right: 0,
+    zIndex: 1,
   },
-  coinsText: {
+  achievementButton: {
+    marginRight: 16,
+    zIndex: 1,
+  },
+  achievementIcon: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    zIndex: 1,
+  },
+  inventoryButton: {
+    marginRight: 16,
+    zIndex: 1,
+  },
+  inventoryIcon: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    zIndex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    zIndex: 0,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    zIndex: 1,
+  },
+  text: {
     fontSize: 24,
-    fontWeight: 'bold',
-    marginLeft: 8,
+    marginTop: 10,
+    fontWeight: 'bold'
   },
-  itemContainer: {
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+  xpBarContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    width: '80%',
+    height: 20,
+    backgroundColor: '#e0e0e0',
+    borderRadius: 10,
+    marginTop: 20,
+    overflow: 'hidden',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  itemText: {
-    fontSize: 18,
-    marginLeft: 20,
-  },
-  buyButton: {
-    padding: 8,
+  xpBar: {
+    height: '100%',
     backgroundColor: '#76c7c0',
-    borderRadius: 4,
   },
-  disabledButton: {
-    backgroundColor: '#ccc',
-  },
-  buyButtonText: {
-    color: '#fff',
+  xpText: {
+    position: 'absolute',
+    color: 'black',
+    fontSize: 12,
     fontWeight: 'bold',
   },
   navbarContainer: {
     position: 'absolute',
     bottom: 0,
-    width: Dimensions.get('window').width,
-  },
-  itemImage: {
-    width: 50,
-    height: 50,
-    marginRight: 16,
+    width: '100%',
   },
 });
